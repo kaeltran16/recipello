14b73df4ae70eacca76ec90b4e76508813b8ae2b
Kael <piskael03@gmail.com>
2018-10-20 01:05:58 -0400

    Updates

---
+++ precache-manifest.00b731b5cb21fb3cf13078828d1439cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0964b00f7c33d4b3bc0",
+    "url": "/recipello/static/js/runtime~main.f0964b00.js"
+  },
+  {
+    "revision": "f95251cd2bd8f7299811",
+    "url": "/recipello/static/js/main.f95251cd.chunk.js"
+  },
+  {
+    "revision": "36d421fb5bc8749f90b3",
+    "url": "/recipello/static/js/1.36d421fb.chunk.js"
+  },
+  {
+    "revision": "f95251cd2bd8f7299811",
+    "url": "/recipello/static/css/main.58570da1.chunk.css"
+  },
+  {
+    "revision": "c04b4547634db3f1976a81ad7fbdf45d",
+    "url": "/recipello/index.html"
+  }
+];
